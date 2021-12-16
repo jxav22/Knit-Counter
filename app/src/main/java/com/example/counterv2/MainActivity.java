99992827110199112f83dc6bf11fcca7ba75fdbb
ggeretsae <94942712+ggeretsae@users.noreply.github.com>
2021-12-17 00:28:27 +1300

    First commit

---
+++ app/src/main/java/com/example/counterv2/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.counterv2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+    private int counter = 0;
+
+    private void setDisplay(int value){
+        final TextView display = (TextView)findViewById(R.id.display);
+        display.setText(String.format("%d", value));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //initialise display
+        setDisplay(counter);
+    }
+
+    public void incrementDisplay(View view){
+        counter++;
+        setDisplay(counter);
+    }
+
+    public void decrementDisplay(View view){
+        counter--;
+        setDisplay(counter);
+    }
+}
