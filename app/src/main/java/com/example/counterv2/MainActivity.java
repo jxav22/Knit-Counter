1269b613b59ec35ab8667b7c00e0f0b1d2adba9d
ggeretsae <94942712+ggeretsae@users.noreply.github.com>
2021-12-17 01:07:08 +1300

    added easter egg to display upper bound

--- app/src/main/java/com/example/counterv2/MainActivity.java
+++ app/src/main/java/com/example/counterv2/MainActivity.java
@@ -10,8 +10,12 @@ public class MainActivity extends Activity {
     private int counter = 0;
 
     private void setDisplay(int value){
+        this.setDisplay(String.format("%d", value));
+    }
+
+    private void setDisplay(String string){
         final TextView display = (TextView)findViewById(R.id.display);
-        display.setText(String.format("%d", value));
+        display.setText(string);
     }
 
     @Override
@@ -27,6 +31,9 @@ public class MainActivity extends Activity {
         if (counter < 1000) {
             counter++;
             setDisplay(counter);
+        } else {
+            // easter-egg
+            setDisplay(":D");
         }
     }
 
