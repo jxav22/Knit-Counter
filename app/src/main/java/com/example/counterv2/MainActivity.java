6c2a40a2c306a2a361531d1d6c6ddb5311902d85
ggeretsae <94942712+ggeretsae@users.noreply.github.com>
2021-12-24 17:55:35 +1300

    updated buttons to increment/decrement fast on long press

--- app/src/main/java/com/example/counterv2/MainActivity.java
+++ app/src/main/java/com/example/counterv2/MainActivity.java
@@ -11,12 +11,12 @@ Make a cool icon for it
 
 package com.example.counterv2;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 import android.support.wearable.activity.WearableActivity;
 
@@ -40,6 +40,33 @@ public class MainActivity extends WearableActivity {
     }
 
 
+    private void storeCounter(){
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt("counter", counter);
+        editor.apply();
+    }
+
+
+    public void incrementDisplay(){
+        if (counter < 999) {
+            counter++;
+            setDisplay(counter);
+            storeCounter();
+        } else {
+            // easter-egg
+            setDisplay(":D");
+        }
+    }
+
+
+    public void decrementDisplay(){
+        if (counter > 0) {
+            counter--;
+            setDisplay(counter);
+            storeCounter();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,35 +83,28 @@ public class MainActivity extends WearableActivity {
         // load counter
         counter = sharedPref.getInt("counter", 0);
 
-        //initialise display
+        // initialise display
         setDisplay(counter);
-    }
 
-    private void storeCounter(){
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putInt("counter", counter);
-        editor.apply();
-    }
+        // initialise buttons
+        Button decrementButton = findViewById(R.id.decrementButton);
+        Button incrementButton = findViewById(R.id.incrementButton);
 
+        decrementButton.setOnTouchListener(new RepeatListener(5000, 100, new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // the code to execute repeatedly
+                decrementDisplay();
+            }
+        }));
 
-    public void incrementDisplay(View view){
-        if (counter < 999) {
-            counter++;
-            setDisplay(counter);
-            storeCounter();
-        } else {
-            // easter-egg
-            setDisplay(":D");
-        }
-    }
-
-
-    public void decrementDisplay(View view){
-        if (counter > 0) {
-            counter--;
-            setDisplay(counter);
-            storeCounter();
-        }
+        incrementButton.setOnTouchListener(new RepeatListener(5000, 100, new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // the code to execute repeatedly
+                incrementDisplay();
+            }
+        }));
     }
 
 }
